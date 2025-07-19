3257ed0c6246360d12f944740b633b64aaf66787
MarkivNazar <nazarmn2008@gmail.com>
2025-07-19 16:10:51 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,7 @@
-import './App.css'
+import './App.css';
 import React from 'react';
 
-function App() {
-
-  const [movies, setMovies] = React.useState<Movie[]>([]);
-
-
-
-  interface Movie {
+interface Movie {
   id: number;
   title: string;
   genre_ids: number[];
@@ -15,50 +9,33 @@ function App() {
   poster_path: string;
 }
 
-interface Genre {
-  id: number;
-  name: string;
-}
+function App() {
+  const [movies, setMovies] = React.useState<Movie[]>([]);
 
   async function fetchPopularMovies(): Promise<Movie[]> {
-  const res = await fetch(
-    `https://api.themoviedb.org/3/movie/popular?api_key=c4d2c6fc867d1a65d5382f3f2424e1aa`
-  );
-  const data = await res.json();
-  console.log(data.results);
-  return data.results;
-}
-
-console.log(fetchPopularMovies());
-
-setMovies([]);
+    const res = await fetch(
+      `https://api.themoviedb.org/3/movie/popular?api_key=c4d2c6fc867d1a65d5382f3f2424e1aa`
+    );
+    const data = await res.json();
+    return data.results;
+  }
 
   React.useEffect(() => {
-    fetchPopularMovies().then(setMovies);
+    fetchPopularMovies().then(setMovies).catch(console.error);
   }, []);
-  
-
-
-
 
   return (
-    <>
     <div>
       <h1>Popular Movies</h1>
-
-      <ul> 
+      <ul>
         {movies.map((movie) => (
           <li key={movie.id}>
             <h2>{movie.title}</h2>
-            </li>
+          </li>
         ))}
       </ul>
-
     </div>
-
-    
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
